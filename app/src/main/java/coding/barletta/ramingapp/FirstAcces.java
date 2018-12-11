40efeae4e425135f9def1c3bababf0bdfbcbe5f3
Francesco Napolitano <franconapa96@gmail.com>
2018-12-11 12:53:57 +0100

    Removed ActionBar. Added Animation File.

--- app/src/main/java/coding/barletta/ramingapp/FirstAcces.java
+++ app/src/main/java/coding/barletta/ramingapp/FirstAcces.java
@@ -2,12 +2,18 @@ package coding.barletta.ramingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 public class FirstAcces extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_acces);
+
     }
 }
